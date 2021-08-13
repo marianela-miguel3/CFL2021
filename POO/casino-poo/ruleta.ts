23e4ca41c0d06315669f3c2e98c5c379da908e92
marianela-miguel3 <nelaecopol@gmail.com>
2021-08-12 21:53:38 -0300

    casino cambios

--- POO/casino-poo/ruleta.ts
+++ POO/casino-poo/ruleta.ts
@@ -15,7 +15,10 @@ export default class Ruleta {
 
   public getGirar(): void {
     let apuesta:number= RLS.questionInt("Ingrese el monto que desea apostar, presione 0 para finalizar: ");
-    let arregloNumeros: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    if (apuesta<=0){
+      throw new Error("La apuesta debe superar los $0");
+    }
+      let arregloNumeros: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
       21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
     let arregloColores: string[] = ["verde","rojo","negro","rojo","negro","rojo","negro","rojo","negro","rojo","negro","negro","rojo",
     "negro","rojo","negro","rojo","negro","rojo","rojo","negro","rojo","negro","rojo","negro","rojo","negro","rojo","negro","negro",
@@ -43,6 +46,11 @@ export default class Ruleta {
           }
     apuesta= RLS.questionInt("Ingrese el monto que desea apostar, presione 0 para finalizar: ");
     FS.appendFileSync("ruletaGuardarInfo.txt",`Hubo un total de ${premioTotal} pesos en premios y un total de ${cantidadAciertos} cantidad de aciertos\n`);
+    }
+    try{
+    }catch (error){
+      console.log("ocurrio un error esperado");
+      console.log(error);
     }
   }
 
